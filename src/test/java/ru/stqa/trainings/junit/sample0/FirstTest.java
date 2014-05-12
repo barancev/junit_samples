24a3707588a4b33a552c6ba78da584d111bc773e
Alexei Barantsev <barancev@gmail.com>
2014-05-12 18:45:19 +0400

    Step 1. Maven + JUnit

---
+++ src/test/java/ru/stqa/trainings/junit/sample0/FirstTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.trainings.junit.sample0;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+  @Test
+  public void test1() {
+    System.out.println("FirstTest.test1");
+  }
+
+  @Test
+  public void test2() {
+    System.out.println("FirstTest.test2");
+  }
+}
---
+++ src/test/java/ru/stqa/trainings/junit/sample0/SecondTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.trainings.junit.sample0;
+
+import org.junit.Test;
+
+public class SecondTest {
+
+  @Test
+  public void test1() {
+    System.out.println("SecondTest.test1");
+  }
+
+  @Test
+  public void test2() {
+    System.out.println("SecondTest.test2");
+  }
+}
